fbc192f4813a5bfebc3cd8650a141a4a7da97ff7
hebiscus <akikopoland@gmail.com>
2025-03-25 12:14:24 +0100

    feat: create basic create sandwich action

--- app/controllers/sandwiches_controller.rb
+++ app/controllers/sandwiches_controller.rb
@@ -4,5 +4,17 @@ class SandwichesController < ApplicationController
   end
 
   def create
+    sandwich = Sandwich.new(sandwich_params)
+
+    if sandwich.save
+      redirect_to "/"
+    else
+      render :new, locals: { sandwich: sandwich }, status: :unprocessable_entity
+    end
+  end
+
+  private 
+  def sandwich_params
+    params.require(:sandwich).permit(:name, :price)
   end
 end
