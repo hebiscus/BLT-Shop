b2b3fd553dfbcaf63e595f30e4f77b124b750636
hebiscus <akikopoland@gmail.com>
2025-05-13 14:20:01 +0200

    feat: create order model

---
+++ db/migrate/20250513114221_create_orders.rb
@@ -0,0 +1,21 @@
+class CreateOrders < ActiveRecord::Migration[8.0]
+  def up
+    execute <<-SQL
+      CREATE TYPE delivery_method AS ENUM ("self_pickup", "delivery");
+    SQL
+
+    create_table :orders do |t|
+      t.column :delivery_method, :delivery_method, default: "self_pickup", null: false
+      t.datetime :delivery_time, null: false
+
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :orders
+    execute <<-SQL
+      DROP TYPE delivery_method;
+    SQL
+  end
+end
