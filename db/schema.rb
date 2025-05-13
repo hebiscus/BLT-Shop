c7a0308db5c02944c8cb976d300f499834bbe444
hebiscus <akikopoland@gmail.com>
2025-05-13 15:27:29 +0200

    feat: schema migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_25_094156) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_13_122057) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  # Custom types defined in this database.
+  # Note that some types may not work with other database engines. Be careful if changing database.
+  create_enum "delivery_method", ["self_pickup", "delivery"]
+
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.bigint "sandwich_id", null: false
+    t.integer "quantity", default: 1, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["sandwich_id"], name: "index_order_items_on_sandwich_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.enum "delivery_method", default: "self_pickup", null: false, enum_type: "delivery_method"
+    t.datetime "delivery_time", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sandwiches", force: :cascade do |t|
     t.string "name", null: false
@@ -21,4 +42,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_25_094156) do
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_sandwiches_on_name"
   end
+
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "sandwiches"
 end
