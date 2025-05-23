348e83184150e3fed3aebce17dfb7b99fdebdb33
hebiscus <akikopoland@gmail.com>
2025-05-23 14:11:27 +0200

    feat: add order create endpoint

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,25 @@
 class OrdersController < ApplicationController
   def create
+    order_schema = OrderSchema.call(params.require(:order).permit!.to_h)
+
+    if order_schema.success?
+      ActiveRecord::Base.transaction do
+        order = Order.create!(
+          delivery_method: order_schema[:delivery_method],
+          delivery_time: order_schema[:delivery_time]
+        )
+
+        order_schema[:order_items].each do |item|
+          order.order_items.create!(
+            sandwich_id: item[:sandwich_id],
+            quantity: item[:quantity]
+          )
+        end
+
+        render json: {id: order.id}, status: :created
+      end
+    else
+      render json: {errors: order_schema.errors.to_h}, status: :unprocessable_entity
+    end
   end
 end
