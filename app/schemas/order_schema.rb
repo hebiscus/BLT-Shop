506f5d8739149f94738bc54f26ff348ccce6e28a
hebiscus <akikopoland@gmail.com>
2025-05-26 14:20:46 +0200

    feat: add order_status and charged_price to order schema

--- app/schemas/order_schema.rb
+++ app/schemas/order_schema.rb
@@ -3,9 +3,11 @@ require "dry/schema"
 OrderSchema = Dry::Schema.Params do
   required(:delivery_method).filled(:string, included_in?: %w[self_pickup delivery])
   required(:delivery_time).filled(:datetime)
+  required(:order_status).filled(:string, included_in?: %w[pending confirmed in_progress out_for_delivery delivered cancelled])
 
   required(:order_items).array(:hash) do
     required(:sandwich_id).filled(:integer)
-    required(:quantity).filled(:integer, gt?: 0)
+    required(:quantity).filled(:integer, gteq?: 0)
+    required(:charged_price).filled(:integer, gteq?: 0)
   end
 end
