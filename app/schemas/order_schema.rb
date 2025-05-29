9422fe581a74ccf9a5b84553225cdc671f9dc30a
hebiscus <akikopoland@gmail.com>
2025-05-29 15:08:49 +0200

    fix: fix filled type syntax

--- app/schemas/order_schema.rb
+++ app/schemas/order_schema.rb
@@ -2,7 +2,7 @@ require "dry/schema"
 
 OrderSchema = Dry::Schema.Params do
   required(:delivery_method).filled(:string, included_in?: %w[self_pickup delivery])
-  required(:delivery_time).filled(:datetime)
+  required(:delivery_time).filled(:date_time)
   required(:order_status).filled(:string, included_in?: %w[pending confirmed in_progress out_for_delivery delivered cancelled])
 
   required(:order_items).array(:hash) do
