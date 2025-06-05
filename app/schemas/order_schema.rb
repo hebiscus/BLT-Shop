ec15de89935e6e1d15da508ebd343647b2577b92
hebiscus <akikopoland@gmail.com>
2025-06-05 22:39:14 +0200

    feat: delete order items from order schema

--- app/schemas/order_schema.rb
+++ app/schemas/order_schema.rb
@@ -4,10 +4,4 @@ OrderSchema = Dry::Schema.Params do
   required(:delivery_method).filled(:string, included_in?: %w[self_pickup delivery])
   required(:delivery_time).filled(:date_time)
   required(:order_status).filled(:string, included_in?: %w[pending confirmed in_progress out_for_delivery delivered cancelled])
-
-  required(:order_items).array(:hash) do
-    required(:sandwich_id).filled(:integer)
-    required(:quantity).filled(:integer, gteq?: 0)
-    required(:charged_price).filled(:integer, gteq?: 0)
-  end
 end
