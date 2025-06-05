3b79c48a5906104a9f006c1fad1df66a1b31e45d
hebiscus <akikopoland@gmail.com>
2025-06-05 15:40:58 +0200

    feat: add current_cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def current_cart
+    @current_cart ||= begin
+      cart = Cart.find_by(id: session[:cart_id])
+      unless cart
+        cart = Cart.create!
+        session[:cart_id] = cart.id
+      end
+      cart
+    end
+  end
 end
