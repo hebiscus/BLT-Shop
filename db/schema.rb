d461bd64070a8d9e102f6b5a562a1dede484334f
hebiscus <akikopoland@gmail.com>
2025-06-20 13:34:20 +0200

    fix: bring back order items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_05_102006) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_06_094735) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -33,6 +33,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_05_102006) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.bigint "sandwich_id", null: false
+    t.integer "quantity", default: 1, null: false
+    t.integer "charged_price", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["sandwich_id"], name: "index_order_items_on_sandwich_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -61,4 +72,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_05_102006) do
 
   add_foreign_key "cart_items", "carts"
   add_foreign_key "cart_items", "sandwiches"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "sandwiches"
 end
