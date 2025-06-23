14ef8cb52cd68279880d0f08a999d6616dbc5a1c
hebiscus <akikopoland@gmail.com>
2025-06-24 00:29:19 +0200

    feat: add shop sandwich association

--- app/models/sandwich.rb
+++ app/models/sandwich.rb
@@ -1,5 +1,7 @@
 class Sandwich < ApplicationRecord
   has_many :order_items
+  has_many :shop_sandwiches
+  has_many :shops, through: :shop_sandwiches
 
   validates :name, :price, presence: true
 end
