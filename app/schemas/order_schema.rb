70d8df53b8cf2cedff982bd1d8147c4e2639c2b3
hebiscus <akikopoland@gmail.com>
2025-07-03 22:21:13 +0200

    feat: require shop_id for order schema

--- app/schemas/order_schema.rb
+++ app/schemas/order_schema.rb
@@ -3,5 +3,6 @@ require "dry/schema"
 OrderSchema = Dry::Schema.Params do
   required(:delivery_method).filled(:string, included_in?: %w[self_pickup delivery])
   required(:delivery_time).filled(:date_time)
+  required(:shop_id).filled(:integer)
   required(:order_status).filled(:string, included_in?: %w[pending confirmed in_progress out_for_delivery delivered cancelled])
 end
