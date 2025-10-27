0f41bf517d9ca6f3235afece2efaccf1cf78e090
hebiscus <akikopoland@gmail.com>
2025-10-27 23:06:06 +0100

    feat: add repositories folder to autoload

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module BltShop
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.autoload_paths << Rails.root.join("app/repositories")
   end
 end
