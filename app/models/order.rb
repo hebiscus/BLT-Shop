8ab4fa4e97db3232531ffa826ba544db688c6208
hebiscus <akikopoland@gmail.com>
2025-11-26 17:33:08 +0100

    feat/wip: add total price before discount to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,12 @@ class Order < ApplicationRecord
   has_many :order_items
   belongs_to :shop
 
+  # to delete
   def total_amount
+    order_items.sum { |item| item.charged_price * item.quantity }
+  end
+
+  def total_price_before_discount
     order_items.sum { |item| item.charged_price * item.quantity }
   end
 end
